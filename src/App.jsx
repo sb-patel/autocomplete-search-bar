6b565c78b7479bad9e7aa2ae7dd5bbc502fd4ed4
Sandeep <sandeep.patel4488@gmail.com>
2025-02-14 09:20:48 +0530

    Search bar and Result container added with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState([]);
+
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/recipes/search?q=");
+    const json = await data.json();
+    setResult(json?.recipes);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [input])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <h1>Autocomplete Search Bar</h1>
+      <input
+        type='text'
+        className='search-input'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <div className='result-container'>
+        {
+          result.map((r) => {
+              <span className='result' key={r.id}>
+                {r.name}
+              </span>
+          })
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
